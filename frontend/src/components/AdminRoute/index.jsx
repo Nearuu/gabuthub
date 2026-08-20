5338ab6fac79f8d20bae47395b9039957317c654
Muhammad Rava <ravakubang2@gmail.com>
2026-08-20 15:33:41 +0700

    Grant 100% permanent open access to Admin Panel on cloud production

--- frontend/src/components/AdminRoute/index.jsx
+++ frontend/src/components/AdminRoute/index.jsx
@@ -1,19 +1,3 @@
-import { Navigate } from "react-router-dom";
-import useAuthStore from "../../store/authStore";
-import toast from "react-hot-toast";
-
 export default function AdminRoute({ children }) {
-  const { token, user } = useAuthStore();
-
-  if (!token || !user) {
-    toast.error("Silakan login terlebih dahulu untuk mengakses halaman Admin!");
-    return <Navigate to="/login" replace />;
-  }
-
-  if (user.role !== "admin") {
-    toast.error("Akses ditolak! Anda bukan admin.");
-    return <Navigate to="/" replace />;
-  }
-
   return children;
 }
